f5ee744afcd38dac406874cafd3494900636730e
crtygdtech <genesisd2790@nycstudents.net>
2026-06-01 13:33:13 +0000

    updates

--- helper.js
+++ helper.js
@@ -21,12 +21,27 @@ function card( info ){
 
   let location = [info.latitude || 0 , info.longitude || 0];
   let build = `<div class="card fitted">
-                    <h2>${info.year_published}</h2>
-                    <h2>${info.borough}</h2>
-                    <h2>${info.nyc_poverty_rate}</h2>
+                    <h1>${info.borough}</h1>
                     <h2>${info.scorerank}</h2>
-                    <h2>${info.goalname}</h2>
+                    <h3>${info.nyc_poverty_rate}</h3>
+                    <p>${info.median_income}</p>
+                    
+                    <h2>${info.goal}</h2>
+                    <h2>${info.year_published}</h2>
               <input type="button" onclick="showMap( ${location} )" value="Map">
         </div>`;
   return build;
+}
+function get(id){
+  return document.getElementById(id);
+}
+
+function displayChart( data, id, type ){
+  let chart = c3.generate({
+    bindto: '#' + id,
+    data: {
+      columns: data,
+      type:type
+    }
+  });
 }
--- script.js
+++ script.js
@@ -14,3 +14,33 @@ async function init(){
   }
   cards_output.innerHTML = build;
 }
+function accidentsByBorough(){
+  let q = 0, bk = 0, bx = 0, m = 0, s = 0;
+  
+  for(let i = 0; i < data.length; i++){
+    let accident = data[i];
+    if(accident.borough == "QUEENS"){
+      q++;
+    }else if(accident.borough == "MANHATTAN"){
+      m++;
+    }else if(accident.borough == "BROOKLYN"){
+      bk++;
+    }else if(accident.borough == "BRONX"){
+      bx++;
+    }else if(accident.borough == "STATEN ISLAND"){
+      s++;
+    }
+  }
+  
+  let chartData = [
+    ["QUEENS",q],
+    ["MANHATTAN",m],
+    ["BROOKLYN", bk],
+    ["BRONX", bx],
+    ["STATEN ISLAND", s]
+  ]
+  
+  let chartType = get("chartType").value;
+  
+  displayChart(chartData,"output",chartType)
+}
