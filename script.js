cb60d3236c11c290eafedbed32ac61186da12798
crtygdtech <genesisd2790@nycstudents.net>
2026-06-08 13:29:28 +0000

    updates

--- script.js
+++ script.js
@@ -36,32 +36,8 @@ async function chart_init(){
 
   
 }
-function filterByBorough(){
-  let borough= document.getElementById("boroughs").value;
-  let build = "";
-  let ct = 0;
 
-  for(let i = 0; i < data.length; i+=1){
-    let info = data[i];
-    if(info.borough== borough){
-      build += `<div class="fitted card">
-                    <h1>${info.borough}</h1>
-                    <h3>Poverty Rate: ${info.nyc_poverty_rate}</h3>
-                    <p>Median Income: ${info.median_income}</p>
-                    <h2>Score Rank: ${info.scorerank}</h2>
-                    <hr>
-                    <h2>${info.goal}</h2>
-                    <h2>${info.goalfullname}</h2>
-                    <h3>${info.goalfullname}</h3>
-                    <span>${info.year_published}</span>
-                </div>`;
-      ct += 1;
-    }
-  }
-  result.innerHTML = `${ct} Results found.`
-  output.innerHTML = build;
-}
-function filterByPovertyrateOrMedianIncome(){
+function filterByPovertyOrIncome(){
   let poverty_rate = document.getElementById("poverty_rates").value;
   let median_income = document.getElementById("median_incomes").value;
   let build = "";
@@ -87,15 +63,15 @@ function filterByPovertyrateOrMedianIncome(){
   result.innerHTML = `${ct} Results found.`
   output.innerHTML = build;
 }
-function filterByYearpublishedAndGoal(){
-  let year_published = document.getElementById("year_publishes").value;
+function filterByBoroughAndGoal(){
+  let borough = document.getElementById("boroughs").value;
   let goal = document.getElementById("goals").value;
   let build = "";
   let ct = 0;
 
   for(let i = 0; i < data.length; i+=1){
     let info = data[i];
-    if(info.year_published == year_published && info.goal == goal){
+    if(info.borough == borough && info.goal == goal){
       build += `<div class="fitted card">
                     <h1>${info.borough}</h1>
                     <h2>Neighborhood: ${info.neighborhoods}</h2>
@@ -145,31 +121,31 @@ function StatisticsByBorough(){
   
   displayChart(chartData,"output",chartType)
 }
-function StatisticsByCommunityDistrict(){
-  let q = 0, bk = 0, bx = 0, m = 0, s = 0;
+function StatisticsByCommunityEthnicity(){
+  let w = 0, b = 0, a = 0, o = 0, h = 0;
   
   for(let i = 0; i < data.length; i+=1){
     let statistic = data[i];
-    console.log(statistic.neighborhoods)
-    if(statistic.borough == "Queens"){
-      q+=1;
-    }else if(statistic.borough == "Manhattan"){
-      m+=1;
-    }else if(statistic.borough == "Brooklyn"){
-      bk+=1;
-    }else if(statistic.borough == "Bronx"){
-      bx+=1;
-    }else if(statistic.borough == "Staten Island"){
-      s+=1;
+    console.log(statistic.ethnicity)
+    if(statistic.ethnicity == "Perc_white"){
+      w+=1;
+    }else if(statistic.ethnicity == "perc_black"){
+      b+=1;
+    }else if(statistic.ethnicity == "perc_asian"){
+      a+=1;
+    }else if(statistic.ethnicity == "perc_other"){
+      o+=1;
+    }else if(statistic.ethnicity == "perc_hispanic"){
+      h+=1;
     }
   }
   
   let chartData = [
-    ["Queens",bx],
-    ["Manhattan",m],
-    ["Brooklyn", bk],
-    ["Bronx", bx],
-    ["Staten Island", s]
+    ["perc_white",w],
+    ["perc_black",b],
+    ["perc_asian", a],
+    ["perc_other", o],
+    ["perc_hispanic", h]
   ]
   
   let chartType = get("chartType").value;
